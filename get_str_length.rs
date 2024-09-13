6963e267bdf11a6ae7d927a06775a84b5deea520
Muzzaiyyan Hussain <muzzaiyyanhussain@yahoo.com>
2024-09-13 21:25:27 +0530

    Add files via upload

---
+++ get_str_length.rs
@@ -0,0 +1,9 @@
+fn main() {
+  let name = String::from("MuzzaiyyanHussain");
+  let len = get_str_len(name);
+  println!("The length of the string is {}", len)
+}
+
+fn get_str_len(str: String) -> usize {
+  return str.chars().count();
+}
